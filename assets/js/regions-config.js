d0a4137d5d02f0addb67a4aaed3ae992ac7cccb0
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-06-29 11:54:08 +0200

    Modif gitignore

---
+++ assets/js/regions-config.js
@@ -0,0 +1,38 @@
+const REGION_CONFIG = [{
+        name: 'Region 1',
+        description: 'Bah Blah Blha',
+        images: [
+            '/images/1.jpg',
+            '/images/2.jpg',
+            '/images/3.jpg',
+        ],
+        coordinates: {
+            lat: 12.2,
+            lng: 12.3,
+        }
+    },
+    {
+        name: 'Region 2',
+        description: 'Bah Blah Blha',
+        coordinates: {
+            lat: 12.2,
+            lng: 12.3,
+        }
+    },
+    {
+        name: 'Region 3',
+        description: 'Bah Blah Blha',
+        coordinates: {
+            lat: 12.2,
+            lng: 12.3,
+        }
+    },
+    {
+        name: 'Region 4',
+        description: 'Bah Blah Blha',
+        coordinates: {
+            lat: 12.2,
+            lng: 12.3,
+        }
+    }
+]
