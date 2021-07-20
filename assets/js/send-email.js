ffa459ed77361756f67970d5d981f76d055b087f
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2021-07-20 16:00:23 +0200

    Added a different message to user if success|error

--- assets/js/send-email.js
+++ assets/js/send-email.js
@@ -1,4 +1,4 @@
-// Links the 
+// Links the contact form with Email JS
 // Code snippet taken from the Code Institute UCD Resume project and modified to fit the app
 function sendMail(contactForm) {
     emailjs.send("service_0gbqz3i", "template_ouiouibaguette", {
@@ -10,10 +10,38 @@ function sendMail(contactForm) {
         .then(
             function(response) {
                 console.log("SUCCESS - Email sent", response);
+                $('#contact-form-title').text("Thank you!");
+                $('#contact-form-text').text("Your email has been sent, we will get back to you soon!");
+                $('#contact-form-content form').hide();
+
+                resetForm()
             },
             function(error) {
                 console.log("FAILED - The email has not been sent", error);
+                $('#contact-form-title').text("Oops...");
+                $('#contact-form-text').text("It seems something went wrong. We are probably already working on a fix, so try again later!");
+                $('#contact-form-content form').hide();
+
+                resetForm()
             }
         );
     return false; // To block from loading a new page
+}
+
+
+// Reset the form to its original condition
+function resetForm() {
+    setTimeout(function() {
+        modalClosing()
+    }, 4000);
+
+    setTimeout(function() {
+        $('#contact-form-title').text("Contact us");
+        $('#contact-form-text').html('<p id="contact-form-text">Any query?<br>Drop us a message and we will get back to you quickly.</p>');
+        $('#contact-form-content form').show();
+        $('#fname').val('');
+        $('#lname').val('');
+        $('#email').val('');
+        $('#message').val('');
+    }, 5000);
 }
